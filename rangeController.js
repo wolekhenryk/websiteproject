32bd36124f25f21eaf0633bbbbbccccad5b3cf0d
Henryk Wołek <henrykwolek15@gmail.com>
2021-01-13 02:59:43 +0100

    Frontend gotowy

---
+++ rangeController.js
@@ -0,0 +1,53 @@
+//New distance
+
+function newDistance0(val) {
+  document.getElementById("newDistance0val").innerHTML = val;
+}
+
+function newDistance1(val) {
+  document.getElementById("newDistance1val").innerHTML = val;
+}
+
+function newDistance2(val) {
+  document.getElementById("newDistance2val").innerHTML = val;
+}
+
+function newDistance3(val) {
+  document.getElementById("newDistance3val").innerHTML = val;
+}
+
+//Modernised distance
+
+function modernisedDistance0(val) {
+  document.getElementById("modernisedDistance0val").innerHTML = val;
+}
+
+function modernisedDistance1(val) {
+  document.getElementById("modernisedDistance1val").innerHTML = val;
+}
+
+function modernisedDistance2(val) {
+  document.getElementById("modernisedDistance2val").innerHTML = val;
+}
+
+function modernisedDistance3(val) {
+  document.getElementById("modernisedDistance3val").innerHTML = val;
+}
+
+//Overall distance
+
+function overallDistance0(val) {
+  document.getElementById("overallDistance0val").innerHTML = val;
+}
+
+function overallDistance1(val) {
+  document.getElementById("overallDistance1val").innerHTML = val;
+}
+
+function overallDistance2(val) {
+  document.getElementById("overallDistance2val").innerHTML = val;
+}
+
+function overallDistance3(val) {
+  document.getElementById("overallDistance3val").innerHTML = val;
+}
